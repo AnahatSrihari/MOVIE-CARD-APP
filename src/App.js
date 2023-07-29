cc7ad44d2f1bdef0f1bda688629058d816312e56
AnahatSrihari <anahatsrihari@gmail.com>
2023-07-29 11:19:03 +0530

    13 -Displaying Movie List with Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import MovieList from "./MovieList";
 
-import MovieCard from "./Moviecard";
-
-function App() {
-  return (
+export default function App(){
+  return(
     <>
-    <h1> Movie Card App</h1>
-    <MovieCard />
+    <h1>Movie App</h1>
+    <MovieList />
     </>
-  );
-}
 
-export default App;
+  )
+}
